a158fc7d38d02d7e748f2efa8060574469bccd2c
BekLg <bereketlegesse22@gmail.com>
2023-11-03 07:53:52 +0300

    added UTC to recognize ethioopian time.

--- index.js
+++ index.js
@@ -48,37 +48,80 @@ const calendar =new Calendar(bot, {
 });
 
 
+// bot.on("callback_query:data", (ctx) => {
+//     if (ctx.msg.message_id == calendar.chats.get(ctx.chat.id)) {
+//         var res;
+//         res = calendar.clickButtonCalendar(ctx.update.callback_query);
+//         // console.log(ctx.update.callback_query);
+//         if (res !== -1) {
+//             // converting to date format
+//             const selectedTime = res; 
+//             const selectedTimeParts = selectedTime.split(':');
+//             const hours = parseInt(selectedTimeParts[0]);
+//             const minutes = parseInt(selectedTimeParts[1]);
+//             const userWakeUpTime = new Date();
+//             userWakeUpTime.setHours(hours);
+//             userWakeUpTime.setMinutes(minutes);
+
+//             ctx.session.wakeUpTime= userWakeUpTime;
+//             ctx.reply("You selected: " + res);
+//             console.log("selected time: ", ctx.session.wakeUpTime);
+
+//             console.log("time: ", ctx.session.wakeUpTime.getHours());
+//             console.log("minute: ", ctx.session.wakeUpTime.getMinutes());
+
+//         }
+//     }
+// });
+
+// bot.command('settime', ctx => calendar.startTimeSelector(ctx));
+
+// bot.command('checktime', (ctx) =>
+//     ctx.reply("you wake Up time is: "+ ctx.session.wakeUpTime.getHours()+ ":"+  ctx.session.wakeUpTime.getMinutes()+ " in Ethiopian time" )
+//  );
+
+
+
 bot.on("callback_query:data", (ctx) => {
     if (ctx.msg.message_id == calendar.chats.get(ctx.chat.id)) {
         var res;
         res = calendar.clickButtonCalendar(ctx.update.callback_query);
-        // console.log(ctx.update.callback_query);
         if (res !== -1) {
-            // converting to date format
-            const selectedTime = res; 
+            const selectedTime = res;
+
+            // Split the selected time
             const selectedTimeParts = selectedTime.split(':');
             const hours = parseInt(selectedTimeParts[0]);
             const minutes = parseInt(selectedTimeParts[1]);
-            const userWakeUpTime = new Date();
-            userWakeUpTime.setHours(hours);
-            userWakeUpTime.setMinutes(minutes);
 
-            ctx.session.wakeUpTime= userWakeUpTime;
+            // Create a Date object for Ethiopian time
+            const ethiopianTime = new Date(selectedTime);
+            ethiopianTime.setFullYear(new Date().getUTCFullYear()); // Set the year to the current year
+            ethiopianTime.setMonth(new Date().getUTCMonth()); // Set the month to the current month
+            ethiopianTime.setUTCMinutes(ethiopianTime.getUTCMinutes() + minutes);
+            ethiopianTime.setUTCHours(ethiopianTime.getUTCHours() + hours);
+
+            // Convert Ethiopian time to UTC
+            const utcTime = new Date(ethiopianTime.getTime() - ethiopianTime.getTimezoneOffset() * 60000);
+
+            // Store the UTC time in the session
+            ctx.session.wakeUpTime = utcTime;
+
+            // Reply to the user with the selected time
             ctx.reply("You selected: " + res);
-            console.log("selected time: ", ctx.session.wakeUpTime);
-
-            console.log("time: ", ctx.session.wakeUpTime.getHours());
-            console.log("minute: ", ctx.session.wakeUpTime.getMinutes());
-
+            console.log("Selected time (Ethiopian time):", ethiopianTime);
+            console.log("Selected time (UTC time):", utcTime.toISOString());
         }
     }
 });
 
 bot.command('settime', ctx => calendar.startTimeSelector(ctx));
 
-bot.command('checktime', (ctx) =>
-    ctx.reply("you wake Up time is: "+ ctx.session.wakeUpTime.getHours()+ ":"+  ctx.session.wakeUpTime.getMinutes()+ " in Ethiopian time" )
- );
+bot.command('checktime', (ctx) => {
+    // Convert UTC time to Ethiopian time for display
+    const ethiopianTime = new Date(ctx.session.wakeUpTime);
+    ctx.reply("Your wake-up time is: " + ethiopianTime.getUTCHours() + ":" + ethiopianTime.getUTCMinutes() + " (Ethiopian time)");
+});
 
 
 
