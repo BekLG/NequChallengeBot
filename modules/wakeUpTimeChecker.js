602b1a8904147c4fbd3cfb93ac94a9320aeadee9
BekLg <bereketlegesse22@gmail.com>
2023-11-17 08:15:14 +0300

    small bug fix

--- modules/wakeUpTimeChecker.js
+++ modules/wakeUpTimeChecker.js
@@ -1,24 +1,34 @@
+const {InlineKeyboard } = require("grammy");
+// const {AskQuestions}= require('') // import ask questions function
 
-
+const inlineKeyboard = new InlineKeyboard()
+  .text("Yes!", "AskQuestions");
 
 // Your function to check and trigger when it's time to wake up
 function checkAndTriggerWakeUp(ctx) {
     // Get the current UTC time
     console.log("checkAndTriggerWakeUp started");
-    const currentTime = new Date();
+// Get the current time as a JavaScript Date object
 
-    // Assume you have the user's UTC wake-up time stored in the session
-    const userWakeUpTime = ctx.session.wakeUpTime;
+const currentTime = new Date();
+
+    //user's UTC wake-up time stored in the session
+    const savedUserWakeUpTime = ctx.session.wakeUpTime;
+
+    // Convert the saved time string to a JavaScript Date object
+const userWakeUpTime = new Date(savedUserWakeUpTime);
+
 
     // Compare the current time with the user's wake-up time
-    if (currentTime.getTime() === userWakeUpTime.getTime()) {
+    if( currentTime.getHours()-6 == userWakeUpTime.getHours() && currentTime.getMinutes() === userWakeUpTime.getMinutes()) {
         // It's time to wake up, trigger your wake-up function here
         console.log("It's time to wake up!");
-        // Call your wake-up function or send a wake-up message to the user
-        // Example: ctx.reply("Good morning! It's time to start your day.");
+        ctx.reply("It's time to Wake Up!! \n\n  Are You Awake?", { reply_markup: inlineKeyboard,});
     }
     else{
         console.log("it is not the time for: ", ctx.session.wakeUpTime)
+
+        console.log("current time:", currentTime.getHours()-6,":", currentTime.getMinutes() ,"saved time:  ",userWakeUpTime.getHours(), ":", userWakeUpTime.getMinutes());
     }
 }
 
