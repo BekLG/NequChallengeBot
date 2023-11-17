6ab4f7c2432e89ee3821f6a1bd0d6dbe30092866
BekLg <bereketlegesse22@gmail.com>
2023-11-17 08:15:38 +0300

    added a function to generate 5 simple math questions

---
+++ modules/generateMathQuestions.js
@@ -0,0 +1,28 @@
+function generateMathQuestions() {
+    const questions = [];
+
+    for (let i = 0; i < 5; i++) {
+        const num1 = Math.floor(Math.random() * 7) + 4;
+        const num2 = Math.floor(Math.random() * 7) + 4;
+        const num3 = Math.floor(Math.random() * 7) + 4;
+
+        const operator1 = '*';
+        const operator2 = '+';
+
+        const question = `${num1} ${operator1} ${num2} ${operator2} ${num3}`;
+
+        // Calculate the answer for the question
+        const answer = eval(question);
+
+        // Store the question and its answer as an object in the array
+        questions.push({
+            question: question,
+            answer: answer,
+        });
+    }
+
+    return questions;
+}
+
+
+module.exports = {generateMathQuestions};
