013cf797cf87b489ed1e36769215b2e244e12a52
BekLg <bereketlegesse22@gmail.com>
2024-02-17 10:09:31 +0300

    clean up

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const bot = new Bot(process.env.BOT_TOKEN);
 
 bot.use(session({ initial: () => ({ 
     id: "", // User ID
-    pointsEarned: 10, // Points Earned, initially 10
+    pointsEarned: 0, // Points Earned, initially 0
     successfulWakeUps: 0, // Successful Wake-Ups
     missedWakeUps: 0, // Missed Wake-Ups
   }) }));
@@ -26,8 +26,6 @@ bot.command('help', (ctx) => {
     const helpMessage = `
     Welcome to Niku challenge Bot! Here are the available commands:
     - /start: Begin the setup process.
-    - /settime: Set or update your wake-up time.
-    - /checktime: Check your current wake-up time.
     - /motivate: Receive a motivational message.
     - /statistics: View your challenge statistics.
   
@@ -37,61 +35,42 @@ bot.command('help', (ctx) => {
 
 
 
+// bot.on("callback_query:data", (ctx) => {
+//     if (ctx.msg.message_id == calendar.chats.get(ctx.chat.id)) {
+//         var res;
+//         res = calendar.clickButtonCalendar(ctx.update.callback_query);
+//         if (res !== -1) {
+//             const selectedTime = res;
 
-const calendar =new Calendar(bot, {
-    date_format: 'HH:mm',
-    language: 'en',
-    bot_api: "grammy",
-    time_range: "02:25-3:30",
-    time_step: "1m"
-});
+//             // Split the selected time
+//             const selectedTimeParts = selectedTime.split(':');
+//             const hours = parseInt(selectedTimeParts[0]);
+//             const minutes = parseInt(selectedTimeParts[1]);
 
+//             const wakeupTime = new Date();
+//             wakeupTime.setHours(hours);
+//             wakeupTime.setMinutes(minutes);
 
-
-bot.on("callback_query:data", (ctx) => {
-    if (ctx.msg.message_id == calendar.chats.get(ctx.chat.id)) {
-        var res;
-        res = calendar.clickButtonCalendar(ctx.update.callback_query);
-        if (res !== -1) {
-            const selectedTime = res;
-
-            // Split the selected time
-            const selectedTimeParts = selectedTime.split(':');
-            const hours = parseInt(selectedTimeParts[0]);
-            const minutes = parseInt(selectedTimeParts[1]);
-
-            const wakeupTime = new Date();
-            wakeupTime.setHours(hours);
-            wakeupTime.setMinutes(minutes);
-
-            ctx.session.wakeUpTime = wakeupTime;
+//             ctx.session.wakeUpTime = wakeupTime;
 
 
-            // Reply to the user with the selected time
-            ctx.reply("You selected: " + res);
-            // console.log("Selected time (Ethiopian time):", ethiopianTime);
-            console.log("Selected time:", wakeupTime.toTimeString());
+//             // Reply to the user with the selected time
+//             ctx.reply("You selected: " + res);
+//             // console.log("Selected time (Ethiopian time):", ethiopianTime);
+//             console.log("Selected time:", wakeupTime.toTimeString());
 
-            setInterval(() => checkAndTriggerWakeUp(ctx, bot), 60 * 1000);
+            
 
-        }
-    }
-});
-
-bot.command('settime', ctx => calendar.startTimeSelector(ctx));
-bot.command('checktime', (ctx) => {
-    // Convert UTC time to Ethiopian time for display
-    const ethiopianTime = new Date(ctx.session.wakeUpTime);
-    ctx.reply("Your wake-up time is: " + ethiopianTime.getUTCHours() + ":" + ethiopianTime.getUTCMinutes() + " (Ethiopian time)");
-});
+//         }
+//     }
+// });
 
 
+bot.command("start",(ctx)=> {
+    setInterval(() => checkAndTriggerWakeUp(ctx, bot), 60 * 1000);
 
-
-
-
-
-bot.command("start",(ctx)=> ctx.reply("hello there, i am niku bot"));
+    ctx.reply("hello there, i am niku bot");
+} )
 
 
 // Catch errors and log them
