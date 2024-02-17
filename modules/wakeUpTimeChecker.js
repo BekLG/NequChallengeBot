0f3fcfbefc6cc023d70012c4a88e1380cce3c74c
BekLg <bereketlegesse22@gmail.com>
2024-02-17 09:41:21 +0300

    some changes

--- modules/wakeUpTimeChecker.js
+++ modules/wakeUpTimeChecker.js
@@ -5,7 +5,15 @@ const inlineKeyboard = new InlineKeyboard()
   .text("Yes!", "AskQuestions");
 
 // Your function to check and trigger when it's time to wake up
-function checkAndTriggerWakeUp(ctx) {
+
+function checkAndTriggerWakeUp(ctx, bot) {
+
+     /////// call back query
+     bot.callbackQuery('AskQuestions', (ctx) => {
+        console.log("inside AskQuestions");
+        ctx.reply("here are 5 questions to prove that you are awake.");
+        });
+
     // Get the current UTC time
     console.log("checkAndTriggerWakeUp started");
 // Get the current time as a JavaScript Date object
@@ -18,12 +26,13 @@ const currentTime = new Date();
     // Convert the saved time string to a JavaScript Date object
 const userWakeUpTime = new Date(savedUserWakeUpTime);
 
-
     // Compare the current time with the user's wake-up time
     if( currentTime.getHours()-6 == userWakeUpTime.getHours() && currentTime.getMinutes() === userWakeUpTime.getMinutes()) {
         // It's time to wake up, trigger your wake-up function here
         console.log("It's time to wake up!");
         ctx.reply("It's time to Wake Up!! \n\n  Are You Awake?", { reply_markup: inlineKeyboard,});
+
+       
     }
     else{
         console.log("it is not the time for: ", ctx.session.wakeUpTime)
@@ -32,11 +41,8 @@ const userWakeUpTime = new Date(savedUserWakeUpTime);
     }
 }
 
-// Set the interval to check every minute (adjust the interval as needed)
-// const interval = setInterval(checkAndTriggerWakeUp(ctx), 10 * 1000); // 60 seconds x 1000 milliseconds = 1 minute
 
-// Stop the interval if needed (e.g., when the bot is stopped or the user changes their wake-up time)
-// clearInterval(interval); // Call this when you want to stop checking
+
 
 
 module.exports = {checkAndTriggerWakeUp};
