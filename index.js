0647c90f362e0ed6f7de4faf71916011c2cba3f3
BekLg <bereketlegesse22@gmail.com>
2024-02-19 12:34:11 +0300

    some mods

--- index.js
+++ index.js
@@ -2,26 +2,36 @@ require("dotenv").config();
 const express=   require("express");
 const { Bot, session } = require("grammy");
 const { run } = require("@grammyjs/runner");
-const { conversations, createConversation } = require("@grammyjs/conversations");
+// const { conversations, createConversation } = require("@grammyjs/conversations");
 // const { keyboard } = require('./modules/keyboards');
 const {checkAndTriggerWakeUp} = require('./modules/wakeUpTimeChecker.js')
 const { RedisAdapter } = require('@grammyjs/storage-redis'); // Import RedisAdapter
 const IORedis = require('ioredis');
-const  Calendar = require('telegram-inline-calendar');
 
 
 // Create an instance of the bot
 const bot = new Bot(process.env.BOT_TOKEN);
 
 
+
 bot.use(session({ initial: () => ({ 
     id: "", // User ID
-    pointsEarned: 0, // Points Earned, initially 0
+    pointsEarned: 10, // Points Earned, initially 0
     successfulWakeUps: 0, // Successful Wake-Ups
     missedWakeUps: 0, // Missed Wake-Ups
   }) }));
 
 
+
+  bot.command("start",(ctx)=> {
+
+    console.log("start clicked");
+    ctx.reply("hello there, i am niku bot");
+  
+      checkAndTriggerWakeUp(ctx, bot); //check immediately, for the first time
+      setInterval(() => checkAndTriggerWakeUp(ctx, bot), 60 * 1000); // check the time every minute.    
+  } )
+  
 bot.command('help', (ctx) => {
     const helpMessage = `
     Welcome to Niku challenge Bot! Here are the available commands:
@@ -66,13 +76,7 @@ bot.command('help', (ctx) => {
 // });
 
 
-bot.command("start",(ctx)=> {
-    checkAndTriggerWakeUp(ctx, bot); //check immediately, for the first time
-    setInterval(() => checkAndTriggerWakeUp(ctx, bot), 60 * 1000); // check the time every minute.
 
-
-    ctx.reply("hello there, i am niku bot");
-} )
 
 
 // Catch errors and log them
