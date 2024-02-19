1cf69f1df31b5914bbc8320ac5e28385a53465f4
BekLg <bereketlegesse22@gmail.com>
2024-02-19 12:33:49 +0300

    able to ask and manage questions by convo

--- modules/askQuestions.js
+++ modules/askQuestions.js
@@ -1,43 +1,39 @@
 // Import the function directly without destructuring
 const generateMathQuestions = require('./mathQuestions');
 
-
-async function askQuestions(ctx, bot) {
-    // Call the function
+// Call the function
 const mathQuestions = generateMathQuestions();
-// Use mathQuestions array as needed
-    console.log("Math questions: ",mathQuestions);
 
+async function askQuestions(conversation, ctx) {
+    
+    // Use mathQuestions array as needed
+    console.log("Math questions: ", mathQuestions);
+
+    // Iterate through each question
     for (let i = 0; i < mathQuestions.length; i++) {
-        const question = mathQuestions[i].question;
-        const answer = mathQuestions[i].answer;
-        var response="";
-
-        // Send the question to the user
-        await ctx.reply(question);
-        
-        // console.log("ctx: ", ctx);
-        // console.log("ctx.update: " , ctx.update.callback_query.message.text);
-        // console.log("ctx.message.text: " , ctx.message.text);
-
-        // Wait for the user's response
-
-        bot.on("message", async (ctx) => {
-            response = await ctx.message.text;       
-          });
-
-          // Check if the response is correct
-        if (parseInt(response.text) === answer) {
-            await ctx.reply("Correct! Let's move to the next question.");
-        } else {
-            await ctx.reply("Incorrect. Please try again with the correct answer.");
-            // Decrement i to repeat the current question
-            i--;
+        let isCorrect = false; // Flag to track if the answer is correct
+        // Repeat the question until the correct answer is provided
+        while (!isCorrect) {
+            // Send the question to the user
+            await ctx.reply(mathQuestions[i].question);
+            // Wait for the user's response
+            const response = await conversation.form.text();
+            // Check if the response is correct
+            console.log(parseInt(response)," : ", mathQuestions[i].answer);
+            if (parseInt(response) == mathQuestions[i].answer) {
+                isCorrect = true;
+                await ctx.reply("Correct! Let's move to the next question.");
+            } else {
+                await ctx.reply("Incorrect. Please try again with the correct answer.");
+            }
         }
-        
     }
     
     await ctx.reply("Congratulations! You've answered all questions correctly.");
+    conversation.session.pointsEarned +=1;
+    conversation.session.successfulWakeUps +=1;
+
+    console.log("points earned: ",conversation.session.pointsEarned);
 }
 
-module.exports = askQuestions;
+module.exports = { askQuestions };
